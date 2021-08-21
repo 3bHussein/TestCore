e4c41599fbde70363ab28d8caf51f35f577295c0
3bHussein <39864308+3bHussein@users.noreply.github.com>
2021-08-21 13:46:20 +0200

    Update Folder Add Angular folder Files

---
+++ DatingApp/my-dream-app/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boo',
+  templateUrl: './app.component.html',
+  // templateUrl: './app.component copy.html',
+
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // title = 'my-dream-app';
+  title = 'Dating App';
+
+}
